f950207470fc3e701dbdf84d7f7701f3317aaf84
Jerry Wang <x.jerry.wang@gmail.com>
2023-07-30 15:30:40 +0800

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.New()
+
+	r.Run(":" + port)
+}
